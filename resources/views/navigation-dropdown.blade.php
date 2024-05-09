f3d34e13a1268198bc1cdd628b20d0c65814e871
creador <angelo.jalp@gmail.com>
2024-05-09 17:47:24 -0400

    trabajando en articulos

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -16,6 +16,13 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('articulo.home') }}" :active="request()->routeIs('articulo.home')">
+                        {{ __('Inventario') }}
+                    </x-jet-nav-link>
+                </div>
+
                 {{-- @can('task_access')
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <x-jet-nav-link href="{{ route('tasks.index') }}" :active="request()->routeIs('tasks.*')">
@@ -52,11 +59,13 @@
                 </div>
                 @endcan
                 @can('user_access')
+                {{--
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link >
                         Inventario
                     </x-jet-nav-link>
                 </div>
+                --}}
                 @endcan
                 @can('user_access')
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
@@ -175,6 +184,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-jet-responsive-nav-link href="{{ route('articulo.home') }}" :active="request()->routeIs('articulo.home')">
+                {{ __('Inventario') }}
             </x-jet-responsive-nav-link>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Livewire\ArticulosController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +20,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/', [TasksController::class, ''])->name('');
+    
+    Route::get('/', [UsersController::class, ''])->name('');
+
+    Route::get('/articulos', [ArticulosController::class, 'home'])->name('articulo.home');
+
+});
+
+/*
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
@@ -25,4 +42,9 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('tasks', \App\Http\Controllers\TasksController::class);
 
     Route::resource('users', \App\Http\Controllers\UsersController::class);
-});
+
+    Route::resource('articulos', \App\Http\Livewire\Articulos::class);
+    //Route::get('/',function(){
+      //  return view('homearti');
+    //});
+});*/
