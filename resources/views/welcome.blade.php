2ba2d1dff5997143493610c48ec494d2fd3a7e14
GBAC22 <gbac2208@gmail.com>
2024-09-26 13:24:55 -0400

    nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Jenecheru</title>
+  <title>Race Store</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sacramento&display=swap">
   <style>
     body {
